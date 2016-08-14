6ec3d4a02bd1a04697ec5d667aab785314192fad
J-Ibarra <ibarrajj@gmail.com>
2016-08-14 12:36:08 -0430

    Video 2

--- src/com/jcs/Main.java
+++ src/com/jcs/Main.java
@@ -2,18 +2,45 @@ package com.jcs;
 
 import static org.lwjgl.glfw.GLFW.*;
 
+import static org.lwjgl.opengl.GL.*;
+
+import static org.lwjgl.opengl.GL11.*;
+
 public class Main {
 
     public Main() {
-        glfwInit();
+        if (!glfwInit()) {
+            System.err.println("GLFW Failed to initialize!");
+            System.exit(0);
+        }
 
-        long win = glfwCreateWindow(640,480,"Elagant Whelp",0,0);
+        long win = glfwCreateWindow(640, 480, "Elagant Whelp", 0, 0);
 
         glfwShowWindow(win);
+        glfwMakeContextCurrent(win);
 
-        while(!glfwWindowShouldClose(win)){
+        createCapabilities();
+
+        while (!glfwWindowShouldClose(win)) {
             glfwPollEvents();
+
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            glBegin(GL_QUADS);
+            glColor3f(1, 0, 0);
+            glVertex2f(-0.5f, 0.5f);
+            glColor3f(0, 1, 0);
+            glVertex2f(0.5f, 0.5f);
+            glColor3f(0, 0, 1);
+            glVertex2f(0.5f, -0.5f);
+            glColor3f(1, 1, 0);
+            glVertex2f(-0.5f, -0.5f);
+            glEnd();
+
+            glfwSwapBuffers(win);
         }
+
+        glfwTerminate();
     }
 
     public static void main(String[] args) {
