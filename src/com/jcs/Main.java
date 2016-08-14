0e9d7fcb0c676464aff8471ca70046508bf06629
J-Ibarra <ibarrajj@gmail.com>
2016-08-14 12:15:28 -0430

    Video 1

--- src/com/jcs/Main.java
+++ src/com/jcs/Main.java
@@ -1,8 +1,22 @@
 package com.jcs;
 
+import static org.lwjgl.glfw.GLFW.*;
+
 public class Main {
 
+    public Main() {
+        glfwInit();
+
+        long win = glfwCreateWindow(640,480,"Elagant Whelp",0,0);
+
+        glfwShowWindow(win);
+
+        while(!glfwWindowShouldClose(win)){
+            glfwPollEvents();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello Game!");
+        new Main();
     }
 }
